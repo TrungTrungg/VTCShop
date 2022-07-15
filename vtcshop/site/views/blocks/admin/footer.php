9dba1d9424594401a8c4f8ee31da47ac35390959
TrungTrungg <xuantrung97tn@gmail.com>
2022-07-15 15:31:47 +0700

    new update

--- vtcshop/site/views/blocks/admin/footer.php
+++ vtcshop/site/views/blocks/admin/footer.php
@@ -5,6 +5,7 @@
             <a href="https://www.instagram.com/trungtrung103/" target="_blank"><i class="fa fa-instagram icon"></i></a>
             <a href="https://github.com/trungtrungg" target="_blank"><i class="fa fa-github icon"></i></a>
         </div>
+        <p>DESIGN BY TrungTrung; DEVELOPED BY TrungTrung, TaiChanh, VanViet</p>
         <p>Copyright &copy; 2020</p>
     </div>
 </div>
--- vtcshop/site/views/blocks/clients/footer.php
+++ vtcshop/site/views/blocks/clients/footer.php
@@ -8,10 +8,10 @@
         <p>&copy Copyright by VTC</p>
     </div>
     <div class="text-2">
-        <p>HỆ THỐNG CỬA HÀNG:
+        <p>CD19CNTT2
         </p>
-        <p>SHOWROOM HCM (Làm việc từ 10h00 - 18h00)<br /> - Địa chỉ 1: 78-80-82 Hoàng Hoa Thám, Phường 12, Quận Tân Bình.<br /> - Địa chỉ 2: 905 Kha Vạn Cân, Phường Linh Tây, Thành phố Thủ Đức.<br /> SHOWROOM HN (Làm việc từ 9h00 - 19h00)<br /> - Địa
-            chỉ : 37 Ngõ 121 Thái Hà, Phường Trung Liệt, Quận Đống Đa.<br />
+        <p>DESIGN BY TrungTrung<br /> 
+        DEVELOPED BY TrungTrung, TaiChanh, VanViet<br /> 
         </p>
     </div>
 </div>
